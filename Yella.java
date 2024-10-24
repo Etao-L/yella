e10c52f68f46297c0bf0e89ff142384765766170
Krishnananda LS <laishrametao@gmail.com>
2024-10-24 22:40:10 +0530

    Just a line

--- Yella.java
+++ Yella.java
@@ -1,9 +1,25 @@
+import java.util.*;
+
 public class Yella {
 	public static void main(String[] args){
 		{
-		System.out.println("Hello World");
+			System.out.println("Hello World");
 		}
 		
+		{
+			System.out.print("Choose: ");
+			System.out.print("\n Searching 1");
+			Scanner sc = new Scanner(System.in);
+			int choice = sc.nextInt();
+			
+			switch(choice){
+			case 1:
+			    System.out.println(" Hey : " + choice);
+			    break;
+			default:
+			    System.out.println("No choice made, going to default. ");
+			}
+		}
 	}
 
 }
